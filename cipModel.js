03bcef6a02e3613ac7a5001fcf5e8c4efdf0f3cf
Aaron <aaron.pillert@arkansas.gov>
2018-09-26 15:16:44 -0500

    creating a model for ajax calls

---
+++ cipModel.js
@@ -0,0 +1,30 @@
+const _appConfig = new WeakMap();
+
+class CIPMODEL {
+    constructor() {
+        _appConfig.set(this, {
+            "_scripts": {
+                "Admin": [
+                    "projectView.js"
+                ],
+                "Planner": [
+                    "projectView.js"
+                ]
+            }
+        });
+    }
+
+    get scripts() {
+        return _appConfig.get(this)._scripts;
+    }
+
+    post(url, params) {
+        let promise = dhx.ajax.post(url, params, (text) => { });
+        return promise;
+    }
+
+    get(url) {
+        let promise = dhx.ajax.get(url, (text) => { });
+        return promise;
+    }
+}
